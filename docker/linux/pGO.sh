434d512c83ab81c1eeb93bc0e8a93b17219983ab
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-18 15:01:31 -0400

    aa

---
+++ docker/linux/pGO.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+source LOG.sh
+
+log_enter pGO
+
+source pENV.sh
+
+CONT=$PROJ_CONT
+CONT_DIR=$PROJ_CONT_DIR
+HOST=$PROJ_HOST
+HOST_DIR=$(pwd)$PROJ_HOST_DIR
+IMG=$PROJ_IMG
+LABEL=$PROJ_LABEL
+NET=$PROJ_NET
+PASSWORD=$PROJ_PASSWORD
+PORT_EXT=$PROJ_PORT_EXT
+PORT_INT=$PROJ_PORT_INT
+USER=$PROJ_USER
+VOL=$PROJ_VOL
+VOL_DIR=$PROJ_VOL_DIR
+
+DB_NAME=$PROJ_DB_NAME
+
+log_var CONT $CONT
+log_var CONT_DIR $CONT_DIR
+log_var HOST $HOST
+log_var HOST_DIR $HOST_DIR
+log_var IMG $IMG
+log_var LABEL $LABEL
+log_var NET $NET
+log_var PASSWORD $PASSWORD
+log_var PORT_EXT $PORT_EXT
+log_var PORT_INT $PORT_INT
+log_var USER $USER
+log_var VOL $VOL
+log_var VOL_DIR $VOL_DIR
+
+log_var DB_NAME $DB_NAME
+
+docker run \
+       -d \
+       -p $HOST:$PORT_EXT:$PORT_INT \
+       -e POSTGRES_DB=$DB_NAME \
+       -e POSTGRES_USER=$USER \
+       -e POSTGRES_PASSWORD=$PASSWORD \
+       --mount type=volume,src=$VOL,dst=$VOL_DIR \
+       --label=$LABEL \
+       --name=$CONT \
+       --network=$NET \
+       $IMG
+
+log_exit pGO
---
+++ env/linux/pENV.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+source LOG.sh
+
+log_enter pENV
+
+source sENV.sh
+
+export PROJ_LABEL=$SYS
+export PROJ_NET=$SYS_NET
+
+export PROJ_CONT=$GIS_CONT
+export PROJ_CONT_DATA_DIR=$GIS_DATA_DIR
+export PROJ_CONT_DIR=$GIS_CONT_DIR
+export PROJ_CONT_HOME_DIR=$GIS_CONT_HOME_DIR
+export PROJ_HOST=$GIS_HOST
+export PROJ_HOST_DIR=$GIS_HOST_DIR
+export PROJ_IMG=$GIS_IMG
+export PROJ_PASSWORD=$GIS_PASSWORD
+export PROJ_PORT_EXT=$GIS_PORT_EXT
+export PROJ_PORT_INT=$GIS_PORT_INT
+export PROJ_USER=$GIS_USER
+export PROJ_VOL=$GIS_VOL
+export PROJ_VOL_DIR=$GIS_VOL_DIR
+
+export PROJ_DB_NAME=$GIS_DB_NAME
+
+log_exit pENV
