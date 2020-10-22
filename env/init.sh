adcaad272368738d33031c6cf9330e2e33c911f9
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-22 12:39:19 -0400

    aa

--- env/init.sh
+++ env/init.sh
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2086
 source LOG.sh
 
-log_enter /Project/env/linux
+log_enter /Project/env/init
 
 cp ./Project/env/linux/*.sh ./Env
 
-log_exit /Project/env/linux
+log_exit /Project/env/init
